bef1bc2d2b15d267ffd7bef92c8bc978dcdfe04b
Ted Strall <tstrall@yahoo.com>
2025-04-21 19:32:12 -0500

    Removing providers

--- common/header/main.tf
+++ common/header/main.tf
@@ -1,14 +1,3 @@
-# Default AWS provider
-provider "aws" {
-  region = var.region
-}
-
-# us-east-1 provider for CloudFront/ACM (optional)
-provider "aws" {
-  alias  = "us_east_1"
-  region = "us-east-1"
-}
-
 data "aws_ssm_parameter" "config" {
   name = "${var.iac_prefix}/${var.component_name}/${var.nickname}/config"
 }
